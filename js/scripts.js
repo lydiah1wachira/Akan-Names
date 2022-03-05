89836cef07a15223e1086c339585898d692e1acd
lydiah1wachira <lydiahcedric@gmail.com>
2022-03-05 23:35:39 +0300

    create a variable for the days

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+let days = ["Sunday", "Monday", "Tuesday", "Wesdnesday", "Thursday", 'Friday', 'Saturday']
+//let maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+//let femaleNames = ['Akasua', ]
