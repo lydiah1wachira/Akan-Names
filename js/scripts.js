0296428c7536f8727f5c0935de5d6fba63cc900b
lydiah1wachira <lydiahcedric@gmail.com>
2022-03-06 23:57:31 +0300

    create objects containing the form elements and create a variable for the submitted form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-
- 
-const {gender, date} = document.forms.birthday_form.elements;
-
+const{gender, date} = document.forms.birthday_form.elements;
 const submitButton = document.getElementById('submit_form')
 
 let modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 let paragraph = document.getElementById("modal_content_p")
 
 
@@ -64,14 +61,10 @@ const datesOfBirth = {
 }
 
 
-// Get the modal
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
